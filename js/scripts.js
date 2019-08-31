1cefa1170619e05618b1398720fe508938b5164c
Tyra-hans <tyrahans17@gmail.com>
2019-08-31 14:29:54 +0300

    add parallax to order now background

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 $(document).ready(function() {
 
-$('.owl-carousel').owlCarousel({
+  $('.owl-carousel').owlCarousel({
     margin:10,
-    loop:true,
     autoWidth:true,
     items:4
-})
-};
+  })
+  $("#topping1").hover(function(){
+    $(".topping1").toggle(500);
+  });
+  $("#topping2").hover(function(){
+    $(".topping2").toggle(500);
+  });
+  $("#topping3").hover(function(){
+    $(".topping3").toggle(500);
+  });
+  $("#topping4").hover(function(){
+    $(".topping4").toggle(500);
+  });
+  $("#topping5").hover(function(){
+    $(".topping5").toggle(500);
+  });
+});
