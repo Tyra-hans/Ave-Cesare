cbebf30259cfa5475d4d479bd2a7e2a9aa54a95b
Tyra-hans <tyrahans17@gmail.com>
2019-08-31 22:51:08 +0300

    add link to card buttons

--- js/scripts.js
+++ js/scripts.js
@@ -20,56 +20,6 @@ $(document).ready(function() {
   $("#topping5").hover(function(){
     $(".topping5").toggle(500);
   });
-});
-$("#add-address").click(function() {
-  $("#new-addresses").append('<div class="new-address">' +
-                               '<div class="form-group">' +
-                                 '<label for="new-street">Street</label>' +
-                                 '<input type="text" class="form-control new-street">' +
-                               '</div>' +
-                               '<div class="form-group">' +
-                                 '<label for="new-city">City</label>' +
-                                 '<input type="text" class="form-control new-city">' +
-                               '</div>' +
-                               '<div class="form-group">' +
-                                 '<label for="new-county">County</label>' +
-                                 '<input type="text" class="form-control new-county">' +
-                               '</div>' +
-                             '</div>');
-});
-$("#new-contact").submit(function(event) {
-  event.preventDefault();
-
-  var inputtedFName = $("#new-name").val();
-  var newContact = new Contact(inputtedName);
-
-  $(".new-address").each(function() {
-    var inputtedStreet = $(this).find(".new-street").val();
-    var inputtedCity = $(this).find(".new-city").val();
-    var inputtedCounty = $(this).find(".new-county").val();
-    var newAddress = new Address(inputtedStreet, inputtedCity, inputtedCounty)
-    newContact.addresses.push(newAddress)
-  });
-
-  $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-  $(".contact").last().click(function() {
-    $("#show-contact").show();
-    $("#show-contact h2").text(newContact.fullName());
-    $(".first-name").text(newContact.firstName);
-    $(".last-name").text(newContact.lastName);
-    $("ul#addresses").text("");
-    newContact.addresses.forEach(function(address) {
-      $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.county + "</li>");
-    });
-  });
-
-  $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input.new-street").val("");
-  $("input.new-city").val("");
-  $("input.new-county").val("");
-  address.prototype.fullAddress= function(){
-    return this.street + this.city + this.county;
-  }
+  var jalapeno= $("#jalapeno").val();
+  
 });
