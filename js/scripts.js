8c8f56e1ab5ac271f40d00ab4d35c1d943afd353
Tyra-hans <tyrahans17@gmail.com>
2019-09-02 08:31:14 +0300

    delete carousel

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,4 @@
-$(document).ready(function() {
-  $('.owl-carousel').owlCarousel({
-    margin:10,
-    autoWidth:true,
-    items:4
-  })
-  $("#topping1").hover(function(){
-    $(".topping1").toggle(500);
-  });
-  $("#topping2").hover(function(){
-    $(".topping2").toggle(500);
-  });
-  $("#topping3").hover(function(){
-    $(".topping3").toggle(500);
-  });
-  $("#topping4").hover(function(){
-    $(".topping4").toggle(500);
-  });
-  $("#topping5").hover(function(){
-    $(".topping5").toggle(500);
-  });
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-  })
-});
+
 // getting values for pizza
 function getValue() {
   var getPizza = document.getElementById("pizza7");
